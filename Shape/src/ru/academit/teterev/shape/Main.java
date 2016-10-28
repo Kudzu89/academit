ccd81cdbc5f01646d80cb1fc13a72d09e78a2e5a
Kudzu89 <kudzu.nsk@gmail.com>
2016-10-29 01:45:06 +0700

    в мэйне написать ф-цию

--- Shape/src/ru/academit/teterev/shape/Main.java
+++ Shape/src/ru/academit/teterev/shape/Main.java
@@ -1,28 +1,28 @@
 package ru.academit.teterev.shape;
 
 public class Main {
+
+    getMaxArea(double area) {
+
+    }
+
     public static void main(String[] args) {
+
         Square square1 = new Square(20);
-        System.out.println("Квадрат:");
-        System.out.printf("%2f площадь %n%2f ширина %n%2f высота %n%2f периметр%n", square1.getArea(), square1.getWidth(), square1.getHeight(), square1.getPerimeter());
-        System.out.println();
+
+        Square square2 = new Square(40);
 
         Triangle triangle1 = new Triangle(12, 10, 11, 22, 16, 7, 10);
-        System.out.println("Треугольник:");
-        System.out.printf("%2f площадь %n%2f ширина %n%2f высота %n%2f периметр%n", triangle1.getArea(), triangle1.getWidth(), triangle1.getHeight(), triangle1.getPerimeter());
-        System.out.println();
+
+        Triangle triangle2 = new Triangle(2, 9, 2, 11, 4, 16, 2);
 
         Rectangle rectangle1 = new Rectangle(22, 44);
-        System.out.println("Прямоугольник:");
-        System.out.printf("%2f площадь %n%2f ширина %n%2f высота %n%2f периметр%n", rectangle1.getArea(), rectangle1.getWidth(), rectangle1.getHeight(), rectangle1.getPerimeter());
-        System.out.println();
+
+        Rectangle rectangle2 = new Rectangle(30, 50);
 
         Circle circle1 = new Circle(55);
-        System.out.println("Окружность:");
-        System.out.printf("%2f площадь %n%2f ширина %n%2f высота %n%2f периметр%n", circle1.getArea(), circle1.getWidth(), circle1.getHeight(), circle1.getPerimeter());
-        System.out.println();
 
-        // git troubles
+        Circle circle2 = new Circle(80);
 
 
     }
