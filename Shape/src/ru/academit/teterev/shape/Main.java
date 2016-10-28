d70f8950a38f4bb94be8b62dc1a7a38289a32353
Kudzu89 <kudzu.nsk@gmail.com>
2016-10-29 01:45:58 +0700

    в мэйне написать ф-цию

--- Shape/src/ru/academit/teterev/shape/Main.java
+++ Shape/src/ru/academit/teterev/shape/Main.java
@@ -6,24 +6,22 @@ public class Main {
 
     }
 
-    public static void main(String[] args) {
 
-        Square square1 = new Square(20);
+    Square square1 = new Square(20);
 
-        Square square2 = new Square(40);
+    Square square2 = new Square(40);
 
-        Triangle triangle1 = new Triangle(12, 10, 11, 22, 16, 7, 10);
+    Triangle triangle1 = new Triangle(12, 10, 11, 22, 16, 7, 10);
 
-        Triangle triangle2 = new Triangle(2, 9, 2, 11, 4, 16, 2);
+    Triangle triangle2 = new Triangle(2, 9, 2, 11, 4, 16, 2);
 
-        Rectangle rectangle1 = new Rectangle(22, 44);
+    Rectangle rectangle1 = new Rectangle(22, 44);
 
-        Rectangle rectangle2 = new Rectangle(30, 50);
+    Rectangle rectangle2 = new Rectangle(30, 50);
 
-        Circle circle1 = new Circle(55);
+    Circle circle1 = new Circle(55);
 
-        Circle circle2 = new Circle(80);
+    Circle circle2 = new Circle(80);
 
 
-    }
 }
